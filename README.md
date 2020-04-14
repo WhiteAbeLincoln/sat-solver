627497608b0a1fe096a8a771127d6023a3e7aa8f
Abraham White <abelincoln.white@gmail.com>
2020-04-13 18:48:04 -0600

    Fix remaining clause counting bug

--- README.md
+++ README.md
@@ -3,10 +3,8 @@ Serial and Parallel implementations of an SAT solver for CS5500
 
 Test with the runtest.sh file. Expects a list of test files in DIMACS format, with optional parameters -t to specify
 the command under test, -c to specify a canonical implementaion to compare with, and -s or -u to specify that the
-result should be SAT or UNSAT respectively.
+result should be SAT or UNSAT respectively. Prints a tab separated list of the failed files and exit codes at the end of 
+execution.
 
 Both commands should exit with code 10 when SAT or code 20 when UNSAT.
 
-Currently not all branching rule heuristic functions can handle the whole test suite. I don't know if this is a bug
-or just inherent to the heuristic. Consequently, it might make more sense to use a portfolio parallel solution rather
-than the divide-and-conquer.
--- runtest.sh
+++ runtest.sh
@@ -9,26 +9,31 @@ while getopts "ht:c:su" opt; do
     c) canon="$OPTARG";;
     s) mode="SAT";;
     u) mode="UNSAT";;
-    h) printf "Usage: %s: [-t TESTER] [-c CANONICAL] [-su] FILES...";;
-    ?) printf "Usage: %s: [-t TESTER] [-c CANONICAL] [-su] FILES..."
+    h) printf "Usage: %s: [-t TESTER] [-c CANONICAL] [-su] FILES...\n" "$0"
+       exit 0;;
+    ?) printf "Usage: %s: [-t TESTER] [-c CANONICAL] [-su] FILES...\n" "$0"
        exit -1;;
   esac
 done
 shift $((OPTIND-1))
 
+pcount=0
+fcount=0
+failures=""
 for f in $@; do
+  failed=
   echo ">>>$f"
   $tester < $f
   tr="$?"
   if [[ "$mode" == "SAT" ]]; then
     if [[ "$tr" -ne "10" ]]; then
       echo "Failure, expected SAT, got $tr"
-      exit -1
+      failed="$f	$tr"
     fi
   elif [[ "$mode" == "UNSAT" ]]; then
     if [[ "$tr" -ne "20" ]]; then
       echo "Failure, expected UNSAT, got $tr"
-      exit -1
+      failed="$f	$tr"
     fi
   else
     # minisat doesn't like the trailing %\n0 that our tests have
@@ -36,9 +41,22 @@ for f in $@; do
     cr="$?"
     if [[ "$tr" -ne "$cr" ]]; then
       echo "Failure, expected $cr, got $tr"
-      exit -1
+      failed="$f	$tr"
     fi
+  fi
+  if [[ -z "$failed" ]]; then
+    pcount=$((pcount+1))
+  else
+    fcount=$((fcount+1))
+    failures="$failures:$failed"
   fi
 done
 
+echo
+echo "$pcount of $# passed"
+echo "$fcount of $# failed"
+echo "Filename	Exit Code$failures" | tr ':' '\n'
+if [[ "$fcount" -ne 0  ]]; then
+  exit -1
+fi
 # vim: ft=sh
